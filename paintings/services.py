cca1d915acf14177c387a180a3a3524d60b87a52
lasteexile <bbriz020903@gmail.com>
2020-10-25 21:45:14 +0200

    After updating page the painting saves

--- paintings/services.py
+++ paintings/services.py
@@ -1,4 +1,25 @@
+import board
+import neopixel
+import logging
+
+
+logging.basicConfig(level=logging.WARNING)
+pixels = neopixel.NeoPixel(board.D18, 256, auto_write=True)
+
+
+def render(color=0, coord=[]):
+    if not coord == []:
+        if coord[1] % 2 == 0:
+            z = 16 * coord[1] + coord[0]
+        else:
+            z = 16 * coord[1] + (15 - coord[0])
+        pixels[z] = color
+    else:
+        pixels.fill((0, 0, 0))
+
+
 def get_color(max_color, color):
+    color = list(map(int, color.split(', ')))
     if color[0] == color[1] == color[2] == 255:
         color = (0, 0, 0)
     else:
--- paintings/views.py
+++ paintings/views.py
@@ -1,28 +1,35 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
+from .services import get_color, render
 
-from .services import get_color
-from .tasks import render
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        if self.request.session.get('field') is None:
+            self.request.session['field'] = [[[i, j, '255, 255, 255'] for j in range(0, 16)] for i in range(0, 16)]
+            self.request.session.save()
+            render()
         context['field'] = self.request.session.get('field')
-        render()
         return context
-
 
 class UpdatePixelsView(TemplateView):
     template_name = 'index.html'
+
     def get(self, request):
         if request.is_ajax():
-            if request.session.get('field') is None or request.GET.get('clear'):
+            if request.GET.get('clear'):
+                request.session['field'] = [[[i, j, '255, 255, 255'] for j in range(0, 16)] for i in range(0, 16)]
+                request.session.save()
                 render()
             else:
                 pixel_id = list(map(int, request.GET.get('pixel_id').split(',')))
-                color = get_color(25, list(map(int, request.GET.get('color').split(', '))))
-                render(color, pixel_id)
+                color = request.GET.get('color')
+                request.session['field'][pixel_id[0]][pixel_id[1]][2] = color
+                request.session.save()
+                render(get_color(10, color), pixel_id)
             return HttpResponse(200)
         return HttpResponse(404)
